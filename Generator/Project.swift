24af1cfcbfd72f6a2b14df933d06107215e267c1
Andreas Ganske <a.ganske@chaosspace.de>
2025-01-01 21:25:58 +0100

    Use swiftlang organization for swift-syntax and format

--- Generator/Project.swift
+++ Generator/Project.swift
@@ -44,8 +44,8 @@ let project = Project(
         // Any dependency changes must also be reflected in ../Package.swift.
         .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.1.0"),
         .package(url: "https://github.com/kylef/Stencil.git", from: "0.15.1"),
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1"),
-        .package(url: "https://github.com/apple/swift-format.git", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.1"),
+        .package(url: "https://github.com/swiftlang/swift-format.git", from: "600.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/LebJe/TOMLKit.git", from: "0.5.5"),
         .package(url: "https://github.com/tuist/XcodeProj.git", from: "8.15.0"),
--- Package.swift
+++ Package.swift
@@ -29,8 +29,8 @@ let package = Package(
         // Any dependency changes must also be reflected in Generator/Project.swift.
         .package(url: "https://github.com/nvzqz/FileKit.git", from: "6.1.0"),
         .package(url: "https://github.com/kylef/Stencil.git", from: "0.15.1"),
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.1"),
-        .package(url: "https://github.com/apple/swift-format.git", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.1"),
+        .package(url: "https://github.com/swiftlang/swift-format.git", from: "600.0.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
         .package(url: "https://github.com/LebJe/TOMLKit.git", from: "0.5.5"),
         .package(url: "https://github.com/tuist/XcodeProj.git", from: "8.15.0"),
